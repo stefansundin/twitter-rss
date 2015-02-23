fa7f9991a22b8aa91b719ecc713c918d2b658226
Stefan Sundin <stefan@stefansundin.com>
2015-02-22 17:46:04 -0800

    Make Cassandra work without ENV for localhost.

--- config/initializers/20-cassandra.rb
+++ config/initializers/20-cassandra.rb
@@ -1,10 +1,12 @@
 require "cassandra"
 
-ENV["CASSANDRA_HOSTS"] ||= "localhost"
-
 begin
-  $cluster = Cassandra.cluster username: ENV["CASSANDRA_USERNAME"], password: ENV["CASSANDRA_PASSWORD"], hosts: ENV["CASSANDRA_HOSTS"].split(",").map(&:strip)
-  $db = $cluster.connect(ENV["CASSANDRA_KEYSPACE"])
+  opts = {}
+  opts[:username] = ENV["CASSANDRA_USERNAME"] if ENV["CASSANDRA_USERNAME"]
+  opts[:password] = ENV["CASSANDRA_PASSWORD"] if ENV["CASSANDRA_PASSWORD"]
+  opts[:hosts] = ENV["CASSANDRA_HOSTS"].split(",").map(&:strip) if ENV["CASSANDRA_HOSTS"]
+  $cluster = Cassandra.cluster opts
+  $db = $cluster.connect ENV["CASSANDRA_KEYSPACE"]
 rescue => e
   puts "Failed to connect to cassandra cluster!"
   puts e.backtrace
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -36,8 +36,12 @@ after_fork do |server, worker|
 
   # reconnect Cassandra
   begin
-    $cluster = Cassandra.cluster username: ENV["CASSANDRA_USERNAME"], password: ENV["CASSANDRA_PASSWORD"], hosts: ENV["CASSANDRA_HOSTS"].split(",").map(&:strip)
-    $db = $cluster.connect(ENV["CASSANDRA_KEYSPACE"])
+    opts = {}
+    opts[:username] = ENV["CASSANDRA_USERNAME"] if ENV["CASSANDRA_USERNAME"]
+    opts[:password] = ENV["CASSANDRA_PASSWORD"] if ENV["CASSANDRA_PASSWORD"]
+    opts[:hosts] = ENV["CASSANDRA_HOSTS"].split(",").map(&:strip) if ENV["CASSANDRA_HOSTS"]
+    $cluster = Cassandra.cluster opts
+    $db = $cluster.connect ENV["CASSANDRA_KEYSPACE"]
   rescue => e
     puts "Failed to connect to cassandra cluster!"
     puts e.backtrace
